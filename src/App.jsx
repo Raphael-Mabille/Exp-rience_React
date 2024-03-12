6602a9cfb2947766f4ae63b8bfd9151d03aadac8
Raphael_m <114739950+Raphael-Mabille@users.noreply.github.com>
2024-03-12 14:42:13 +0100

    TicTacToe Tutorial 1h30
    learned:
    passing parameters to handler
    react reacting to other things changing

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ const products = [
     { title: 'Cabbage', isFruit: false, id: 1 },
     { title: 'Garlic', isFruit: false, id: 2 },
     { title: 'Apple', isFruit: true, id: 3 },
-  ];
+];
 
 const listItems = products.map(product =>
     <li key={product.id}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import App from './TicTacToe';
 import reportWebVitals from './reportWebVitals';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
