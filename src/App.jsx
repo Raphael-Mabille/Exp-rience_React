110b9b646afc798d82c9d331021358c44550da24
Raphael_m <114739950+Raphael-Mabille@users.noreply.github.com>
2024-03-12 11:46:54 +0100

    Quickstart react 1h
    learned:
    hooks
    handler
    components
    JSX syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
+const products = [
+    { title: 'Cabbage', isFruit: false, id: 1 },
+    { title: 'Garlic', isFruit: false, id: 2 },
+    { title: 'Apple', isFruit: true, id: 3 },
+  ];
+
+const listItems = products.map(product =>
+    <li key={product.id}
+        className={'text-slate-' + product.id * 100}
+        style={{
+            color: product.isFruit ? 'magenta' : 'darkgreen'
+        }}>
+       {product.title}
+    </li>
+);
+
 function App() {
-  return (
-    <h1>TEST</h1>
-  );
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+
+    const user = {
+        name: 'John Doe',
+        class: 'text-slate-50'
+    };
+    return (
+        <div>
+            <h1 className={user.class}>Welcome to my app {user.name}!</h1>
+            <h2>here's a list of product you may want to buy: {listItems}</h2>
+            <MyButton count={count} onClick={handleClick}/><br></br>
+            <MyButton count={count} onClick={handleClick}/>
+        </div>
+    );
+}
+
+function MyButton({count, onClick}) {
+    return (
+        <button onClick={onClick} >
+            Clicked {count} times
+        </button>
+    );
 }
 
 
